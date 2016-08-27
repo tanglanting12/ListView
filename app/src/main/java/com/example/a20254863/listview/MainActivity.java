e3694b779c51f9f2b464cbe43d5235b8a6ff670e
20254863 <tanting12@163.com>
2016-08-27 11:19:25 +0800

    test git

--- app/src/main/java/com/example/a20254863/listview/MainActivity.java
+++ app/src/main/java/com/example/a20254863/listview/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 new GetDataTask().execute();
             }
         });
-        // Add an end-of-list listener
+        // Add an end-of-list listener aa
         mPullRefreshListView.setOnLastItemVisibleListener(new OnLastItemVisibleListener() {
 
             @Override
